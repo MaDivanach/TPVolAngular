755248cd593f1f7f904234e07aedfdb1577f7171
Unknown <mathilde.divanach@gmail.com>
2018-09-07 17:05:38 +0200

    Débug

--- src/app/route.ts
+++ src/app/route.ts
@@ -1,20 +1,24 @@
 import {Routes} from '@angular/router';
-import {AppComponent} from './app.component';
 import {ReservationComponent} from './reservation/reservation.component';
 import {PassagerComponent} from './passager/passager.component';
 import {ClientComponent} from './client/client.component';
 import {VolComponent} from './vol/vol.component';
 import {PassagerEditComponent} from './passager/passager-edit.component';
 import {ClientEditComponent} from './client/client-edit.component';
+import {ReservationEditComponent} from './reservation/reservation-edit.component';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [{path: 'home', component: AppComponent},
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent},
   {path: 'passager', component: PassagerComponent},
   {path: 'passager/create', component: PassagerEditComponent},
   {path: 'passager/edit/:id', component: PassagerEditComponent},
   {path: 'reservation', component: ReservationComponent},
+  {path: 'reservation/create', component: ReservationEditComponent},
+  {path: 'reservation/edit/:id', component: ReservationEditComponent},
   {path: 'client', component: ClientComponent},
-  {path: 'client/create', component: ClientEditComponent},
-  {path: 'client/edit/:id', component: ClientEditComponent},
+  {path: 'client/create/:type', component: ClientEditComponent},
+  {path: 'client/edit/:type/:id', component: ClientEditComponent},
   {path: 'vol', component: VolComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
